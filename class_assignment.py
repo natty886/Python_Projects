0f9e798c7cb92166dc4fdd30bb3e8187bcd51c3d
natty886 <84761201+natty886@users.noreply.github.com>
2023-05-15 17:51:50 -0700

    inherit classes

---
+++ class_assignment.py
@@ -0,0 +1,12 @@
+class User:
+    name = 'No Name Provided'
+    email = ' '
+
+
+class Employee(User):
+    employee_id = 0
+    organization = 'Marketing'
+
+class Customer(User):
+    mailing_address = ' '
+    mailing_list = True
